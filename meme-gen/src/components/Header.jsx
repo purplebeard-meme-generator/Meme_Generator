83044525dc8983e86ff29a3fd0416b4bc44a2be5
Thiruisa Puvanenthirarasa <thiruisa@hotmail.co.uk>
2022-01-19 18:19:25 +0000

    Initial design

---
+++ meme-gen/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export class Header extends Component {
+    render() {
+        return (
+            <h1>HEADER</h1>
+        )
+    }
+}
+
+export default Header;
