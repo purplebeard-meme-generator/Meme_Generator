234e4a950c0853e36b7a2c8038bdf28c923b0c08
Thiruisa Puvanenthirarasa <thiruisa@hotmail.co.uk>
2022-01-26 17:39:06 +0000

    added capture screen capture and font size

--- meme-gen/src/components/MemeGenerator.jsx
+++ meme-gen/src/components/MemeGenerator.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import '../App.css';
+import Draggable from "react-draggable";
+import { ScreenCapture } from 'react-screen-capture';
 
 export class MemeGenerator extends Component {
     constructor(){
         super();
         this.state = {
+            font_size: "22",
             topText: '',
+            middleText: '',
             bottomText: '',
             randomImg: 'http://i.imgflip.com/1bij.jpg',
             allMemeImgs: []
@@ -13,6 +17,15 @@ export class MemeGenerator extends Component {
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+    handleClick = () => {
+        let randomNumber = Math.floor(
+            Math.random() * this.state.allMemeImgs.length
+        );
+        this.setState({ randomImg: this.state.allMemeImgs[randomNumber].url });
+    };
+
+    increaseFont = () => {};
     // we make an API call  and save the data returned to a new state property 
     componentDidMount(){
         fetch("https://api.imgflip.com/get_memes")
@@ -40,38 +53,78 @@ export class MemeGenerator extends Component {
         this.setState({ randomImg: randMemeImg });
     }
 
-    render() {
-        return (
-            <div className='card'>
-                <form className="meme-form" onSubmit={this.handleSubmit}>
-                    <input 
-                    type="text"
-                    name="topText"
-                    placeholder="Top Text"
-                    value={this.state.topText}
-                    onChange={this.handleChange}
-                    />
-                    <input
-                    type="text"
-                    name="bottomText"
-                    placeholder="Bottom Text"
-                    value={this.state.bottomText}
-                    onChange={this.handleChange}
-                    />
-                    <div className='genBtn'>
-                        <button>Generate</button>
-                </div>
-                </form>
-                
-                <div className='meme'>
-                    <img src={this.state.randomImg} alt='' />
-                    <h2 className='top'>{this.state.topText}</h2>
-                    <h2 className='bottom'>{this.state.bottomText}</h2>
+    handleScreenCapture = screenCapture => {
+        this.setState({screenCapture});
+    };
 
-                    {/* <button className="downloadBtn">Download</button> */}
+    handleSave = () => {
+        const screenCaptureSource = this.state.screenCapture;
+        const downloadLink = document.createElement('a');
+        const fileName = 'react-screen-capture.png';
+
+        downloadLink.href = screenCaptureSource;
+        downloadLink.download = fileName;
+        downloadLink.click();
+    };
+
+    render() {
+        const { screenCapture } = this.state;
+        console.log(this.state.font_size);
+        console.log(screenCapture);
+        return (
+            <ScreenCapture onEndCapture={this.handleScreenCapture}>
+            {({ onStartCapture }) => (
+                <div className='card'>
+                    <form className="meme-form" onSubmit={this.handleSubmit}>
+                        <input 
+                        type="text"
+                        name="topText"
+                        placeholder="Top Text"
+                        value={this.state.topText}
+                        onChange={this.handleChange}
+                        />
+                        <input
+                        type="text"
+                        name="middleText"
+                        placeholder="Middle Text"
+                        value={this.state.middleText}
+                        onChange={this.handleChange}
+                        />
+                        <input
+                        type="text"
+                        name="bottomText"
+                        placeholder="Bottom Text"
+                        value={this.state.bottomText}
+                        onChange={this.handleChange}
+                        />
+                        <input
+                        type="number"
+                        name="font_size"
+                        placeholder="font size"
+                        value={this.state.font_size}
+                        onChange={this.handleChange}
+                        />
+                    </form>
+                    <form className="meme-form" onSubmit={this.handleSubmit}><button className='genBtn' >Generate</button></form>
+                    
+                    <div className='meme'>
+                        <img src={this.state.randomImg} alt='' />
+                        <Draggable><h2 className='top' style={{ fontSize: Number(this.state.font_size) }}>{this.state.topText}</h2></Draggable>
+                        <Draggable><h2 className='middle' style={{ fontSize: Number(this.state.font_size) }}>{this.state.middleText}</h2></Draggable>
+                        <Draggable><h2 className='bottom' style={{ fontSize: Number(this.state.font_size) }}>{this.state.bottomText}</h2></Draggable>
+
+                        <button className='captureBtn' onClick={onStartCapture}>Capture</button>
+
+                    </div>
+                    <center>
+                        <img src={this.state.screenCapture} alt='react-screen-capture' />
+                        <p>
+                            {this.state.screenCapture && <button className='downloadBtn'onClick={this.handleSave}>Download</button>}
+                        </p>
+                    </center>
                 </div>
-                
-            </div>
+            )}
+            </ScreenCapture>
         )       
     }
 }
